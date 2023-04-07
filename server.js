4a4c9be23da26fa98f2bb7204e7cd40a4801e421
Victoria <vmcnorrill@gmail.com>
2023-04-07 18:34:02 -0400

    realized that the dom elements were already coded in the public folder, deleted my work

--- server.js
+++ server.js
@@ -2,17 +2,11 @@
 const express = require('express');
 const path = require('path');
 const middleware = require('./middleware/custommiddleware.js');
-const api = require('./routes/index.js'); // not sure if I'll use this
+const api = require('./routes/routes.js'); // not sure if I'll use this
 
 const PORT = process.env.port || 3001;
 
 const app = express();
-
-//Element Variables with  jQuery
-let inputTitle = $('.note-title').val();
-let inputNote = $('note-textarea').val();
-let saveBtn = $('save-note');
-let newNote = $('new-note');
 
 // Import custom middleware
 app.use(middleware);
@@ -21,13 +15,14 @@ app.use(middleware);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
-
 app.use(express.static('public'));
 
-//event listener for save button
-saveBtn.on('click', function () {
-    //send input to the sever
-    app.get('/', (req, res) => {
-        res.send(`${inputTitle} ${inputNote}`)
-    })
-})
+// routes to route files
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
+
+// app listener - starts the server
+app.listen(PORT, () => {
+  console.log(`Server available at localhost${PORT}`);
+});
