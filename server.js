e42938c8009ac4e893745e86fdb75971e9124bb7
Victoria <vmcnorrill@gmail.com>
2023-04-07 13:02:13 -0400

    added to server.js

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
+//EXPRESS
 const express = require('express');
 const path = require('path');
 const middleware = require('./middleware/custommiddleware.js');
-// const api = require('./routes/index.js'); // not sure if I'll use this
+const api = require('./routes/index.js'); // not sure if I'll use this
 
 const PORT = process.env.port || 3001;
 
 const app = express();
+
+//Element Variables with  jQuery
+let inputTitle = $('.note-title').val();
+let inputNote = $('note-textarea').val();
+let saveBtn = $('save-note');
+let newNote = $('new-note');
 
 // Import custom middleware
 app.use(middleware);
@@ -16,3 +23,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
 
 app.use(express.static('public'));
+
+//event listener for save button
+saveBtn.on('click', function ()
+    //send input to the sever
+    app.get('/', (req, res) => {
+        res.send(`${inputTitle} ${inputNote}`)
+    })
+)
